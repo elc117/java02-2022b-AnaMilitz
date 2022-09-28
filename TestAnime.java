39bfc08a23129fb80494adcf1445d9a16bf1dd4a
AnaMilitz <63315782+AnaMilitz@users.noreply.github.com>
2022-09-27 22:30:19 -0300

    Update TestAnime.java

--- TestAnime.java
+++ TestAnime.java
@@ -1,49 +1,5 @@
-class Anime{
 
-    private string nome;
-    private boolean diasAtuais;
-    private int ano;
-
-    Anime(){
-        nome = "";
-        diasAtuais = false;
-        ano = 0;
-    }
-    
-    public string getNome(){
-        return nome;
-    }
-
-    public boolean getDiasAtuais(){
-        return diasAtuais;
-    } public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-    
-    public int getAno(){
-        return ano;
-    }
-
-    public void setNome( string nome ){
-        this.nome = nome;
-    }
-
-    public void setDiasAtuais( boolean diasAtuais ){
-        this.diasAtuais = diasAtuais;
-    }
-    
-    public void setAno( int ano ){
-        this.ano = ano;
-    }
-
-    public void estaAtivoHoje(){
-        if( diasAtuais )
-        	System.out.println("O anime está sendo produzido nos dias atuais!\n");
-        else
-            System.out.println("O anime não está sendo produzido nos dias atuais!");
-    }
-   
-}
+class TestAnime {
 public static void main(String[] args) {
 	Anime OnePiece = new Anime();
 	Anime SwordArtOnline = new Anime();
@@ -62,5 +18,6 @@ public static void main(String[] args) {
 	OnePiece.setDiasAtuais(true);
 	OnePiece.estaAtivoHoje());
 	SwordArtOnline.estaAtivoHoje();
-}
+        }
 
+}
