2916ba8eff4cce5d9c32693afcb97c208c72a9b4
AnaMilitz <63315782+AnaMilitz@users.noreply.github.com>
2022-10-23 10:57:18 -0300

    Update TestAnime.java

--- TestAnime.java
+++ TestAnime.java
@@ -1,23 +1,28 @@
 
 class TestAnime {
 public static void main(String[] args) {
+	
+	//One Piece
 	Anime OnePiece = new Anime();
-	Anime SwordArtOnline = new Anime();
-	
-	SwordArtOnline.setNome("Sword Art Online");
 	OnePiece.setNome("One Piece");
-	System.out.println("Nome do anime 1: "+OnePiece.getNome());
-	System.out.println("Nome do anime 2: "+SwordArtOnline.getNome());
-	
-	SwordArtOnline.setAno(2300);
 	OnePiece.setAno(2016);
-	System.out.println("Ano do anime 1: "+OnePiece.getAno());
-	System.out.println("Ano do anime 2: "+SwordArtOnline.getAno());
-	
-	SwordArtOnline.setDiasAtuais(true);
 	OnePiece.setDiasAtuais(true);
-	OnePiece.estaAtivoHoje());
-	SwordArtOnline.estaAtivoHoje();
+
+	System.out.println("Nome do anime 1: "+OnePiece.getNome());
+	System.out.println("Ano do anime 1: "+OnePiece.getAno());
+	OnePiece.estaAtivoHoje();
+
+	//Sword Art Online
+	Anime SwordArtOnline = new Anime();
+	SwordArtOnline.setNome("Sword Art Online");
+	SwordArtOnline.setAno(2012);
+	SwordArtOnline.setDiasAtuais(false);
+
+	System.out.println("Nome do anime 2: "+SwordArtOnline.getNome());
+	System.out.println("Ano do anime 2: "+SwordArtOnline.getAno());
+	SwordArtOnline.estaAtivoHoje(); 
+	System.out.println("Obs: Sword Art Online está sendo produzido nos dias atuais. O resultado contrário é apenas para teste do uso de false.");
+	
         }
 
 }
